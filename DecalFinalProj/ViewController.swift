cfb36cce6ad03cd863c3f21aab16e431565cc0e3
Bikramjit Kukreja <bikramjit@berkeley.edu>
2018-11-13 14:36:24 -0800

    Creating StoryBoard

---
+++ DecalFinalProj/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DecalFinalProj
+//
+//  Created by Bikramjit Singh Kukreja on 11/7/18.
+//  Copyright © 2018 Bikramjit Singh Kukreja. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
